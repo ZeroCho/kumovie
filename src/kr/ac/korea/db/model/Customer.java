cc5f485df9df2043e5a98ef20fed4fbf463fc265
ffaass <rlatnghd95@gmail.com>
2017-06-11 16:34:50 +0900

    모델 추가

---
+++ src/kr/ac/korea/db/model/Customer.java
@@ -0,0 +1,77 @@
+package kr.ac.korea.db.model;
+
+import java.util.Date;
+
+/**
+ * Created by ffaass on 2017-06-11.
+ */
+public class Customer {
+    private String customerId;
+    private String password;
+    private String name;
+    private String phoneNum;
+    private Date birthday;
+    private String customerRank;
+
+    public Customer(String customerId,
+                    String password,
+                    String name,
+                    String phoneNum,
+                    Date birthday,
+                    String customerRank) {
+        this.customerId = customerId;
+        this.password = password;
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.birthday = birthday;
+        this.customerRank = customerRank;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCustomerRank() {
+        return customerRank;
+    }
+
+    public void setCustomerRank(String customerRank) {
+        this.customerRank = customerRank;
+    }
+}
---
+++ src/kr/ac/korea/db/model/MovieType.java
@@ -0,0 +1,30 @@
+package kr.ac.korea.db.model;
+
+/**
+ * Created by ffaass on 2017-06-11.
+ */
+public class MovieType {
+    private String type;
+    private int price;
+
+    public MovieType(String type, int price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
